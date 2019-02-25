ee8558a558c488b64834bf7a32ab1cb0de655d3e
johnale <johnale@cisco.com>
2019-02-24 23:47:34 -0500

    emit newmessage to welcome users and alert new user joined

--- src/server/server.js
+++ src/server/server.js
@@ -16,6 +16,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log(`New User connected`);
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined',
+        createdAt: new Date().getTime()
+    })
+
     socket.on('createMessage', (message => {
         console.log(`Received a new message: ${JSON.stringify(message, undefined, 2)}`);
 
@@ -24,6 +35,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //         from: message.from,
+        //         text: message.text,
+        //         createdAt: new Date().getTime()
+        //     })
     }));
 
     socket.on('disconnect', () => {
