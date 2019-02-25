0de59f6102274d957874dfef96a103377f6455d1
johnale <johnale@cisco.com>
2019-02-24 22:23:07 -0500

    added connect and disconnect events

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 
 
 const publicPath = path.join(__dirname, '../../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`nodeChatter started on port: ${port}`));
+io.on('connection', (socket) => {
+    console.log(`New User connected`);
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+
+
+server.listen(port, () => console.log(`nodeChatter started on port: ${port}`));
 
